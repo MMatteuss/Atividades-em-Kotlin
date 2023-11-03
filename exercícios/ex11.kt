2256f66d8cff731d9c4c698c3d17887eb3f9eddf
MMatteuss <mateusmoneito57@gmail.com>
2023-11-03 17:23:40 -0300

    Create ex11.kt

---
+++ exercícios/ex11.kt
@@ -0,0 +1,37 @@
+package execicios
+
+import kotlin.math.pow
+
+fun main(){
+    print("O valor de C ele e negativo? [s/n]")
+    val valoCn: String = readln()!!.toString()
+    if (valoCn == "s"){
+        print("Digite o valor de ax² ")
+        val valorA : Int = readln().toInt()
+        print("Digite o valor de bx ")
+        val valorBint : Int = readln().toInt()
+        val valorB = valorBint.toDouble().pow(2)
+        // valorB: Float
+        print("Digite o valor de c ")
+        readln().toInt()
+
+        val valorDelta = (valorB.pow(2) -4 * (valorA * -valorB))
+
+        println("O valor de Delta e igual á $valorDelta")
+    }else if(valoCn == "n"){
+        print("Digite o valor de ax² ")
+        val valorA : Int = readln().toInt()
+        print("Digite o valor de bx ")
+        val valorBint : Int = readln().toInt()
+        val valorB = valorBint.toDouble().pow(2)
+        // valorB: Float
+        print("Digite o valor de c ")
+        readln().toInt()
+
+        val valorDelta = (valorB.pow(2) -4 * (valorA * (valorB)))
+
+        println("O valor de Delta e igual á $valorDelta")
+    }else{
+        println("Porfavor reinicie o código e coloque S: para SIM, e N: para NÃo")
+    }
+}

3a375f3b8cd3364daa5022f79f90471f1dc5b7f8
MMatteuss <mateusmoneito57@gmail.com>
2023-11-30 19:14:40 -0300

    Update ex61.kt

--- PASSO 06 – REPETIÇÃO COM FAÇA ENQUANTO/ex61.kt
+++ PASSO 06 – REPETIÇÃO COM FAÇA ENQUANTO/ex61.kt
@@ -4,11 +4,12 @@ Crie um programa que mostre na tela a seguinte contagem, usando a estrutura
 “faça enquanto”
 0 3 6 9 12 15 18 21 24 27 30 Acabou!
  */
-fun main(){
-    var contador = -3
-    while (contador < 30){
-        contador+=3
-        print("$contador ")
-    }
+fun main (){
+    var contar = -3
+    do {
+        contar+=3
+        print("$contar ")
+
+    } while (contar < 30)
     print("Acabou!")
 }
